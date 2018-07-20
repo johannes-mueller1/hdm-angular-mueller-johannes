e9ecae6eaac2f7c65041185bb8059acc0de0acde
johannes-mueller1 <jm097@hdm-stuttgart.de>
2018-07-20 08:00:51 +0200

    Configuration Authentication Guard

---
+++ src/app/autentication.guard.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+@Injectable()
+export class AuthenticationGuard implements CanActivate {
+
+    constructor(private router: Router) { }
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+        const url: string = state.url;
+        return this.userIsLoggedIn(url);
+        }
+
+private userIsLoggedIn(url: string): boolean { return true; }
+}
