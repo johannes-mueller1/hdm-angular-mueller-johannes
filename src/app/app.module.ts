b9783b2a9a5d40bcbfeee5d752ca29303b3ae5bd
johannes-mueller1 <jm097@hdm-stuttgart.de>
2018-07-20 08:04:31 +0200

    Update AppComponent and Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { AppRoutingModule } from './/app-routing.module';
+import { SetTokenComponent } from './components/set-token/set-token.component';
+import { HomeComponent } from './components/home/home.component';
+import {AuthenticationGuard} from './autentication.guard';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SetTokenComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      HttpClientModule,
+      AppRoutingModule
   ],
-  providers: [],
+  providers: [AuthenticationGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
